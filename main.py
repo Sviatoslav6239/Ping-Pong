b33004396f7cd3e1c6729fbd1047937a29eaa796
Sviatoslav <svf121209@gmail.com>
2023-09-13 18:47:04 +0200

    Game end

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from random import choice
+
 from pygame import *
 init()
 font.init()
@@ -19,7 +21,55 @@ class GameSprite(sprite.Sprite):
 class Ball(GameSprite):
     def __init__(self):
         super().__init__("images/ball.png", (WIDTH - 50) // 2, (HEIGHT - 50) // 2, 50, 50, 10)
+        self.speed_x = self.speed
+        self.speed_y = self.speed
+        self.disable_frames = 60
+        self.disable = True
 
+    def update(self, left_racket, right_racket):
+        if not self.disable:
+            self.rect.x += self.speed_x
+            self.rect.y += self.speed_y
+            if self.rect.y >= HEIGHT - self.rect.height:
+                self.speed_y *= -1
+            if self.rect.y < 0:
+                self.speed_y *= -1
+
+            if sprite.collide_rect(right_racket, ball):
+                self.speed_x *= -1.1
+                self.rect.x = right_racket.rect.x - self.rect.width
+            if sprite.collide_rect(left_racket, ball):
+                self.speed_x *= -1.1
+                self.rect.x = left_racket.rect.right
+
+            if ball.rect > WIDTH:
+                self.respawn()
+                self.update_score(left_racket.score)
+
+            if ball.rect.x < -self.rect.w:
+                self.respawn()
+                self.update_score(right_racket.score)
+
+        else:
+            self.disable_frames -= 1
+            if self.disable_frames <= 0:
+                self.remove_disable()
+
+    def respawn(self):
+        self.rect.x = (WIDTH - 50) // 2
+        self.rect.y = (HEIGHT - 50) // 2
+        self.speed_x = choice((-self.speed, self.speed))
+        self.speed_y = choice((-self.speed, self.speed))
+
+    def set_disable(self):
+        self.disable = True
+        self.disable_frames = 60
+
+    def remove_disable(self):
+        self.disable = False
+
+    def update_score(self, score):
+        score.inscrease_score(1)
 
 class Player(GameSprite):
     def __init__(self, player_number):
@@ -28,10 +78,59 @@ class Player(GameSprite):
         if self.player_number == 1:
             self.image_name = "images/Line.png"
             self.x = 100
+            self.score = Score(WIDTH // 3, HEIGHT // 20)
         elif self.player_number == 2:
-            self.image_name = "images/Line.png"
+            self.image_name = "images/Line2.png"
             self.x = WIDTH - 100 - 150
+            self.score = Score(WIDTH - WIDTH // 3, HEIGHT // 20)
         super().__init__(self.image_name, self.x, (HEIGHT - 150) // 2, self.width, self.width, 15)
+
+    def update(self):
+        keys_pressed = key.get_pressed()
+        if self.player_number == 1:
+            if keys_pressed[K_w] and self.rect.y >= 10:
+                self.rect.y -= self.speed
+            if keys_pressed[K_s] and self.rect.y <= HEIGHT - self.rect.height - 10:
+                self.rect.y += self.speed
+
+        if self.player_number == 2:
+            if keys_pressed[K_UP] and self.rect.y >= 10:
+                self.rect.y -= self.speed
+                if keys_pressed[K_DOWN] and self.rect.y <= HEIGHT - self.rect.height - 10:
+                    self.rect.y += self.speed
+
+
+
+class Score:
+    def __init__(self, x, y):
+        self.font = font.Font(None, 60)
+        self.x = x
+        self.y = y
+        self.num = 0
+        self.text = str(self.num)
+        self.image = self.font.render(self.text, True, (0, 0, 0))
+
+    def inscrease_score(self, amount):
+        self.num += amount
+        self.text = str(self.num)
+        self.image = self.font.render(self.text, True, (0, 0, 0))
+
+    def drop_score(self):
+        self.num = 0
+        self.text = str(self.num)
+        self.image = self.font.render(self.text, True, (0, 0, 0))
+
+    def reset(self, surface):
+        surface.blit(self.image, (self.x, self.y))
+
+
+    def restart(self):
+        ball.respawn()
+        player1.score.drop_score()
+        player2.score.drop_score()
+        player1.rect.y = (HEIGHT - 150) // 2
+        player2.rect.y = (HEIGHT - 150) // 2
+
 
 
 WIDTH = 1280
@@ -52,33 +151,33 @@ player1 = Player(1)
 player2 = Player(2)
 
 game = True
-finish  = False
+finish = False
 while game:
     for e in event.get():
         if e.type == QUIT:
             game = False
         if e.type == VIDEORESIZE:
             new_width = e.w
-            new_height = int(new_width / ASPECT_RATIO )
+            new_height = int(new_width / ASPECT_RATIO)
             window = display.set_mode((new_width, new_height), RESIZABLE)
             current_size = window.get_size()
 
     virtual_surface.fill((139, 180, 228))
 
     ball.reset()
+    ball.update(player1, player2)
+
 
     player1.reset()
+    player1.update()
+
     player2.reset()
+    player2.update()
+
+    player1.score.reset(virtual_surface)
+    player2.score.reset(virtual_surface)
 
     scaled_surface = transform.scale(virtual_surface, current_size)
     window.blit(scaled_surface, (0, 0))
     display.update()
     clock.tick(FPS)
-
-
-
-
-
-
-
-
